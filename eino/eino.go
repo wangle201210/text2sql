d7d54cfac709a0c5870f7fc8631bd65630f4ba70
wanna <wanna.w@binarywalk.com>
2025-04-01 15:05:57 +0800

    fix: 有些模型会返回sql tag，需要清理

--- eino/eino.go
+++ eino/eino.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 func GetSQL(ddl, question string) (sql string, err error) {
@@ -21,7 +22,8 @@ func GetSQL(ddl, question string) (sql string, err error) {
 	if err != nil {
 		return "", fmt.Errorf("生成SQL失败: %w", err)
 	}
-	return result.Content, nil
+	sql = result.Content
+	return trimSql(sql), nil
 }
 
 func ChoiceSQL(sqls, ddl, question string) (sql string, err error) {
@@ -39,7 +41,8 @@ func ChoiceSQL(sqls, ddl, question string) (sql string, err error) {
 	if err != nil {
 		return "", fmt.Errorf("选择SQL失败: %w", err)
 	}
-	return result.Content, nil
+	sql = result.Content
+	return trimSql(sql), nil
 }
 
 func PrettyRes(sql, question string, runResult []map[string]interface{}) (res string, err error) {
@@ -62,4 +65,13 @@ func PrettyRes(sql, question string, runResult []map[string]interface{}) (res st
 		return "", fmt.Errorf("优化回答失败: %w", err)
 	}
 	return result.Content, nil
+}
+
+// 如果有sql tag 就删除
+func trimSql(sql string) string {
+	if strings.HasPrefix(sql, "```sql\n") {
+		sql = strings.TrimLeft(sql, "```sql\n")
+		sql = strings.TrimRight(sql, "\n```")
+	}
+	return sql
 }
